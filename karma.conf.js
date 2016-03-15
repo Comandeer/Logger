a882086fd675da9168d1d0d6c95f70b51feafe98
Comandeer <vepomoc@gmail.com>
2016-03-16 00:28:45 +0100

    Updated tests.

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,11 @@
 // Karma configuration
-// Generated on Mon Mar 14 2016 00:04:51 GMT+0100 (CET)
+// Generated on Tue Mar 15 2016 22:00:35 GMT+0100 (CET)
 
 module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-    client: {
-        useIframe: false
-    },
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
@@ -38,7 +34,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: [ 'progress', 'coverage' ],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
@@ -72,10 +68,7 @@ module.exports = function(config) {
     concurrency: 1,
 
     coverageReporter: {
-        type: 'text-summary',
-        instrumenter: {
-            '**/**.js': 'istanbul'
-        }
+        type: 'text-summary'
     }
   })
 }
--- test/logger.js
+++ test/logger.js
@@ -8,7 +8,6 @@ const testMsg = 'test message';
 
 describe( 'Logger', () => {
 	it( 'is an object', () => {
-
 		expect( logger ).to.be.an( 'object' );
 	} );
 
@@ -30,77 +29,89 @@ describe( 'Logger', () => {
 } );
 
 describe( 'Logger.success', () => {
-	before( () => {
+	beforeEach( () => {
 		sinon.spy( console, 'log' );
-		logger.success( testMsg );
 	} );
 
-	after( () => {
+	afterEach( () => {
 		console.log.restore();
 	} );
 
 	it( 'calls console.log', () => {
+		logger.success( testMsg );
+
 		expect( console.log ).to.have.been.calledOnce;
 	} );
 
 	it( 'prepends the message with the styled OK: phrase', () => {
+		logger.success( testMsg );
+
 		expect( console.log ).to.always.have.been.calledWith( '%c OK:', 'color: green;font-weight: bold;', testMsg );
 	} );
 } );
 
 describe( 'Logger.warning', () => {
-	before( () => {
+	beforeEach( () => {
 		sinon.spy( console, 'warn' );
-		logger.warning( testMsg );
 	} );
 
-	after( () => {
+	afterEach( () => {
 		console.warn.restore();
 	} );
 
 	it( 'calls console.log', () => {
+		logger.warning( testMsg );
+
 		expect( console.warn ).to.have.been.calledOnce;
 	} );
 
 	it( 'prepends the message with the styled Warning: phrase', () => {
+		logger.warning( testMsg );
+
 		expect( console.warn ).to.always.have.been.calledWith( '%c Warning:', 'color: yellow;font-weight: bold;', testMsg );
 	} );
 } );
 
 describe( 'Logger.error', () => {
-	before( () => {
+	beforeEach( () => {
 		sinon.spy( console, 'error' );
-		logger.error( testMsg );
 	} );
 
-	after( () => {
+	afterEach( () => {
 		console.error.restore();
 	} );
 
 	it( 'calls console.log', () => {
+		logger.error( testMsg );
+
 		expect( console.error ).to.have.been.calledOnce;
 	} );
 
 	it( 'prepends the message with the styled Error: phrase', () => {
+		logger.error( testMsg );
+
 		expect( console.error ).to.always.have.been.calledWith( '%c Error:', 'color: red;font-weight: bold;', testMsg );
 	} );
 } );
 
 describe( 'Logger.info', () => {
-	before( () => {
+	beforeEach( () => {
 		sinon.spy( console, 'log' );
-		logger.info( testMsg );
 	} );
 
-	after( () => {
+	afterEach( () => {
 		console.log.restore();
 	} );
 
 	it( 'calls console.log', () => {
+		logger.info( testMsg );
+
 		expect( console.log ).to.have.been.calledOnce;
 	} );
 
 	it( 'prepends the message with the styled Info: phrase', () => {
+		logger.info( testMsg );
+
 		expect( console.log ).to.always.have.been.calledWith( '%c Info:', 'color: blue;font-weight: bold;', testMsg );
 	} );
 } );
